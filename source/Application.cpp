e91a72a33d9d9513d15e70d10fce9e23c0e2a35b
Elisey <elisey.tagan@gmail.com>
2022-05-16 14:18:16 +0300

    Third commit

---
+++ source/Application.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Application.hpp"
+
+namespace Game {
+
+    void sayHello()
+    {
+        std::cout << "Hello from Simple Engine Core" << std::endl;
+    }
+
+}
---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Application.hpp"
+
+int main()
+{
+    std::cout << "Hello from Simple Engine Editor" << std::endl;
+    Game::sayHello();
+    std::cin.get();
+}
